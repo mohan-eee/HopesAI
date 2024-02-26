3b6e516c71b6480c07b2f9f544bdfb71f743b933
mohan-eee <mohan.eee007@gmail.com>
2024-02-26 13:14:02 +0530

    update changes

--- Python/4.input_statement.py
+++ Python/4.input_statement.py
@@ -1,4 +1,9 @@
 num1=input("Enter num1 :")
 num2=input("Enter num2 :")
-add = num1 + num2
-print ("sum is : ", add)
+add=num1+num2
+print ("The answer is : ",add)  
+no1=int(input("Enter first number:"))
+no2=int(input("Enter second number2 :"))
+Addition=no1+no2
+print ("The answer is : ",Addition)  
+ 
